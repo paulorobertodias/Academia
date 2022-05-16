667b78853ceb5888e88b6227945ae0ca84aacb14
robertodias <roberto-dias@bm.rs.gov.br>
2022-05-16 10:24:07 -0300

    Criação do projeto

---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function abrirFalar(){
+    document.getElementById('popup_fale').style.display = 'block';
+}
+function fecharFalar(){
+    document.getElementById('popup_fale').style.display =  'none';
+}
+
+
+function abrirMatricula(){
+    document.getElementById('popup_Matricula').style.display = 'block';
+}
+function fecharMatricula(){
+    document.getElementById('popup_Matricula').style.display =  'none';
+}
+
+
+function mascara(){
+    document.getElementById('txtTelefone').style.display= '(00) 0000-0000'
+}
